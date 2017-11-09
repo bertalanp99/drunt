9a16eac749238e43a9cf8d9cb3e7b031e9fdf96b
bertalanp99 <peter.bertalan.root@gmail.com>
2017-11-10 00:34:00 +0100

    Added a few lines to main

--- drunt.c
+++ drunt.c
@@ -9,6 +9,17 @@
 
 int main(int argc, char *argv[])
 {
+    FILE* fp = fopen("cal.ics", "r");
+    if (fp == NULL)
+    {
+        printf("Fuck! Could not open %s\n", "cal.ics");
+    }
+    else
+    {
+        printf("Hmm.. I seem to be able to open %s\n", "cal.ics");
+    }
+    fclose(fp);
+    
     printf("Reading ICS...\n");
     Calendar cal;
     Calendar_create(&cal);
